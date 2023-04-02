b4962cad0c198ef0734df1aa8339c6bf922e5bdb
Leopoldo Hernandez <loliva@wayfair.com>
2023-04-02 17:07:27 -0500

    added sorting to the main method

--- Ch3_Dynamic_Programming/02_DpGreedyAlgoCreatingAMealPlan/main.py
+++ Ch3_Dynamic_Programming/02_DpGreedyAlgoCreatingAMealPlan/main.py
@@ -36,14 +36,13 @@ def load_nutrient_data(filename):
                     foods_list.append(food)
     except Exception as ex:
         print(f"{ex}")
-    print(foods_list)
-    return []
+    return foods_list
 
 def sort_food_list(foods, nutrient):
     # Sort the food list based on the percent-by-calories of the
     # given nutrient ('protein', 'carbs' or 'fat')
     # The list is sorted in-place; nothing is returned.
-    pass
+    foods.sort(key = attrgetter(nutrient), reverse = True)
 
 def create_meal_plan(foods, nutrient, goal):
     # A greedy algorithm to create a meal plan that has MAX_CALORIES
@@ -65,11 +64,59 @@ if __name__ == "__main__":
     filename = input()
     foods = load_nutrient_data(filename)
 
+    MAX_P: int = 1
+    MAX_C: int = 2
+    MAX_F: int = 3
+    EXIT:  int = 4
+
+    ELIGIBLE_OPTIONS = [
+        MAX_P, MAX_C, MAX_F, EXIT
+    ]
+
+
+    valid_options: dict = {
+        MAX_P: False,
+        MAX_C: False,
+        MAX_F: False,
+        EXIT: False,
+    }
+
+    options_map: dict = {
+        MAX_P: 'protein',
+        MAX_C: 'carbs',
+        MAX_F: 'fat',
+    }
     # 2. Display menu and get user's choice. Repeat menu until a
     # valid choice is entered by the user (1-4, inclusive).
+    try:
+        nutrient = ''
+        option: int = 0
+        while option not in ELIGIBLE_OPTIONS:
+            print_menu()
+            option = int(input())
+        valid_options[option] = True
+    except Exception as ex:
+        print(f"{ex}. That's not a correct input.  Please try again.")
+
 
     # 3. Prompt user for goal nutrient percent value. Repeat prompt
     # until a valid choice is entered by the user (0-100, inclusive)
+
+    if valid_options[EXIT]:
+        exit()
+    else:
+        nutrient: str = '';
+        valid_options.pop(4)
+        for key, option in valid_options.items():
+            if option:
+                nutrient = options_map[key];
+    try:
+        goal = int(input("What percentage of calories from fat is the goal? "))
+        while goal < 0 or goal > 100:
+            print(f"Percent given is {goal}. Please be sure to select a percentage between 0-100")
+            goal = int(input("What percentage of calories from fat is the goal? "))
+    except Exception as ex:
+        print(f"{ex}. Please be sure to select a percentage between 0-100")
 
     # 4. Run greedy algorithm to create the meal plan.
     # plan = createMealPlan(foods, nutrient, goal)
