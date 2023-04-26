4e6e7ae264166dcade8caf04cd95a762d827da6e
Leopoldo Hernandez <loliva@wayfair.com>
2023-04-25 22:25:02 -0500

    changing models to use the @dataclass decorator

--- Project/models/Package.py
+++ Project/models/Package.py
@@ -1,6 +1,7 @@
 from typing import Any
+from dataclasses import dataclass
 
-
+@dataclass
 class Package:
 
     """A class to create a package object that then gets
@@ -18,93 +19,12 @@ class Package:
         notes: Any additional notes about the package.
     """
 
-    def __init__(
-        self,
-        package_id: str,
-        address: str,
-        city: str,
-        state: str,
-        zipcode: str,
-        delivery_time: str,
-        weight: str,
-        status: str,
-        notes: str
-    ) -> None:
-        self.package_id: str = package_id
-        self.address: str = address
-        self.city: str = city
-        self.state: str = state
-        self.zipcode: str = zipcode
-        self.delivery_time: str = delivery_time
-        self.weight: str = weight
-        self.status: str = status
-        self.notes: str = notes
-
-    """
-    Complexity: O(1)
-    """
-    def set_id(self, package_id: str) -> None:
-        self.package_id = package_id
-
-    """
-    Complexity: O(1)
-    """
-    def set_address(self, address: str) -> None:
-        self.address = address
-
-    """
-    Complexity: O(1)
-    """
-    def set_city(self, city: str) -> None:
-        self.city = city
-
-    """
-    Complexity: O(1)
-    """
-    def set_state(self, state: str) -> None:
-        self.state = state
-
-    """
-    Complexity: O(1)
-    """
-    def set_zipcode(self, zipcode: str) -> None:
-        self.zipcode = zipcode
-
-    """
-    Complexity: O(1)
-    """
-    def set_delivery_time(self, delivery_time: str) -> None:
-        self.delivery_time = delivery_time
-
-    """
-    Complexity: O(1)
-    """
-    def set_status(self, status: str) -> None:
-        self.status = status
-
-    """
-    Complexity: O(1)
-    """
-    def set_notes(self, notes: str) -> None:
-        self.notes = notes
-
-    """
-    Complexity: O(1)
-    """
-    def __repr__(self) -> str:
-    """Returns a string representation of the package object.
-
-    Returns:
-        A string representation of the package object.
-    """
-        return (
-            f"Package(package_id='{self.package_id}', "
-            f"address='{self.address}', "
-            f"city='{self.city}', "
-            f"state='{self.state}', "
-            f"zipcode='{self.zipcode}', "
-            f"delivery_time='{self.delivery_time}', "
-            f"weight='{self.weight}', "
-            f"status='{self.status}', "
-            f"notes='{self.notes}')"
-        )
+    package_id: str
+    address: str
+    city: str
+    state: str
+    zipcode: str
+    delivery_time: str
+    weight: str
+    status: str
+    notes: str
--- Project/models/Truck.py
+++ Project/models/Truck.py
@@ -1,6 +1,7 @@
 from typing import List
+from dataclasses import dataclass
 
-
+@dataclass
 class Truck:
 
     """A class used to create a truck object that will hold a package object.
@@ -10,59 +11,11 @@ class Truck:
         speed: The speed of the truck in miles per hour.
         miles: The number of miles the truck has traveled.
         capacity: The maximum capacity of the truck in terms of the number of packages it can hold.
-        status: The current status of the truck, such as "At HUB" or "Out for Delivery".
+        status: The current status of the truck, such as "AT HUB" or "Out for Delivery".
     """
 
-    def __init__(self, capacity: int = 16) -> None:
-        self.truck: List[Package] = []
-        self.speed: float = 18
-        self.miles: float = 0
-        self.capacity: int = capacity
-        self.status: str = 'AT HUB'
-
-    """
-    Complexity: O(1)
-    """
-    def add_package(self, package: Package) -> None:
-        """Adds a package to the truck.
-
-        Args:
-            package: The package to add to the truck.
-        """
-        if len(self.truck) < self.capacity:
-            self.truck.append(package)
-        else:
-            raise ValueError('Truck is full.')
-
-    """
-    Complexity: O(1)
-    """
-    def remove_package(self, package: Package) -> None:
-        """Removes a package from the truck.
-
-        Args:
-            package: The package to remove from the truck.
-        """
-        self.truck.remove(package)
-
-    """
-    Complexity: O(1)
-    """
-    def add_miles(self, miles: float) -> None:
-        """Adds miles to the truck's odometer.
-
-        Args:
-            miles: The number of miles to add to the odometer.
-        """
-        self.miles += miles
-
-    """
-    Complexity: O(1)
-    """
-    def set_status(self, status: str) -> None:
-        """Sets the truck's status.
-
-        Args:
-            status: The new status of the truck.
-        """
-        self.status = status
+    truck: List[Package]
+    speed: float
+    miles: float
+    capacity: int
+    status: str
